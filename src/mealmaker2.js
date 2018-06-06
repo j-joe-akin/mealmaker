3e6741c099f302d19be96f2a5ab6c60ac886a7e0
j-joe-akin <jjfour@gmail.com>
2018-06-06 07:30:52 -0400

    changing object hierarchy- dayOfWeek at top

--- src/mealmaker2.js
+++ src/mealmaker2.js
@@ -1,31 +1,91 @@
-let weekDay = {
-  _menus: {
+let daysOfWeek = {
+  _monday: {
+    mains: [],
+    sides: []
+  },
+  _tuesday: {
+    mains: [],
+    sides: []
+  },
+  _wednesday: {
+    mains: [],
+    sides: []
+  },
+  _thursday: {
+    mains: [],
+    sides: []
+  },
+  _friday: {
+    mains: [],
+    sides: []
+  },
+  _saturday: {
+    mains: [],
+    sides: []
+  },
+  _sunday: {
     mains: [],
     sides: []
   },
   get mains() {
     return;
   },
-  set mains(mainIn) {
+  get sides() {
     return;
   },
-  get sides() {
+  set mains(mainIn) {
     return;
   },
   set sides(sideIn) {
     return;
   },
-  get _menus() {
+  get monday() {
     return {
-      mains: this._menus.mains;
-      sides: this._menus.sides;
+      mains: this._monday.mains,
+      sides: this._monday.sides
     };
   },
-  addDishToMenu(courseName, dishName, dishPrice) {
+  get tuesday() {
+    return {
+      mains: this._tuesday.mains,
+      sides: this._tuesday.sides
+    };
+  },
+  get wednesday() {
+    return {
+      mains: this._wednesday.mains,
+      sides: this._wednesday.sides
+    };
+  },
+  get thursday() {
+    return {
+      mains: this._thursday.mains,
+      sides: this._thursday.sides
+    };
+  },
+  get friday() {
+    return {
+      mains: this._friday.mains,
+      sides: this._friday.sides
+    };
+  },
+  get saturday() {
+    return {
+      mains: this._saturday.mains,
+      sides: this._saturday.sides
+    };
+  },
+  get sunday() {
+    return {
+      mains: this._sunday.mains,
+      sides: this._sunday.sides
+    };
+  },
+  addDishToCourses(dayOfWeek, courseName, dishName, dishPrice) {
     const dish = {
       _name: dishName,
       _price: dishPrice
     };
-    this._menus[courseName].push(dish);
-  },
+    
+  }
 }
